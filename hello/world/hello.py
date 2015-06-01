1ed184df6154ef04dac83321187ed555a9ab3c9c
Alex Clark <aclark@aclark.net>
2015-05-31 21:48:36 -0400

    Update

---
+++ hello/world/hello.py
@@ -0,0 +1,11 @@
+s is hello/world/hello.py
+from zope.publisher.browser import BrowserPage as View
+
+
+class Hello(View):
+    """
+    View class and methods. Access by opening /<VIEW>{/<METHOD>} e.g. /hello{/world}
+    """
+
+    def world(self):
+        return("Hello, World!")
